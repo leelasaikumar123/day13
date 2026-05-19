2823e35a807c6b775bd37e190d492b794d39d6c9
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-19 12:46:31 +0530

    feat:findind max ele from float

--- Min&Max/MinAndMax.java
+++ Min&Max/MinAndMax.java
@@ -4,9 +4,20 @@ public class MinAndMax{
        Integer b=90;
        Integer c=100; 
        findMax(a, b, c);
+       findMax(10.0f,11.0f,12.0f);
     }
     public static void findMax(Integer num1,Integer num2,Integer num3){
         Integer greatest=num1;
+        if(num2.compareTo(greatest)>0){
+           greatest=num2;
+        }
+                if(num3.compareTo(greatest)>0){
+           greatest=num3;
+        }
+        System.out.println(greatest);
+    }
+        public static void findMax( Float num1,Float num2,Float num3){
+        Float greatest=num1;
         if(num2.compareTo(greatest)>0){
            greatest=num2;
         }
