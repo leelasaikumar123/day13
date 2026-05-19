865db6da155dcca35d3d9ca78c693032037c1569
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-19 13:53:44 +0530

    feat:multiple values

--- Min&Max/MinAndMax.java
+++ Min&Max/MinAndMax.java
@@ -1,38 +1,25 @@
 public class MinAndMax{
     public static void main(String[] args) {
-        MaximumTest<Integer> intObj = new MaximumTest<>(10, 20, 30);
+            Integer[] intArr = {10, 20, 30, 40, 50};
+        Float[] floatArr = {11.2f, 54.3f, 8.7f, 99.1f};
+        String[] stringArr = {"Apple", "Peach", "Banana", "Zoo"};
+        System.out.println("Maximum Integer: " + MaximumTest.testMaximum(intArr));
+        System.out.println("Maximum Float: "  + MaximumTest.testMaximum(floatArr));
 
-        System.out.println("Maximum Integer: " + intObj.testMaximum());
-
-        MaximumTest<Float> floatObj =new MaximumTest<>(11.2f, 54.3f, 8.7f);
-
-        System.out.println("Maximum Float: "  + floatObj.testMaximum());
-
-        MaximumTest<String> stringObj = new MaximumTest<>("Apple", "Peach", "Banana");
-
-        System.out.println("Maximum String: "+ stringObj.testMaximum());
+        System.out.println("Maximum String: "+ MaximumTest.testMaximum(stringArr));
     }
 }
  class MaximumTest<T extends Comparable<T>>{
-  T x;
-   T y;
-   T z;
-   public MaximumTest(T x, T y, T z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-   }
-   public T testMaximum(){
-    return testMaximum(x,y,z);
-   } 
-   public static <T extends Comparable<T>> T testMaximum(T x, T y, T z){
-        T greatest=x;
-        if(y.compareTo(greatest)>0){
-           greatest=y;
+ 
+   public static <T extends Comparable<T>> T testMaximum(T[] values){
+T greatest = values[0];
+
+  for (T value : values) {
+         if (value.compareTo(greatest) > 0) {
+                greatest = value;
+            }
         }
-        if(z.compareTo(greatest)>0){
-           greatest=z;
-        }
+
         return greatest;
    }
  }   
