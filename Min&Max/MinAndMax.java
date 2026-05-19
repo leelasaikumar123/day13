cf1a211200b02871d0be636080a4cc55c047320d
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-19 13:18:14 +0530

    feat:using generic methods

--- Min&Max/MinAndMax.java
+++ Min&Max/MinAndMax.java
@@ -7,8 +7,8 @@ public class MinAndMax{
        findMax(10.0f,11.0f,12.0f);
        findMax("Leela","Sai","Kumar");
     }
-    public static void findMax(Integer num1,Integer num2,Integer num3){
-        Integer greatest=num1;
+    public static <T extends Comparable<T>> void findMax(T num1,T num2,T num3){
+        T greatest=num1;
         if(num2.compareTo(greatest)>0){
            greatest=num2;
         }
@@ -17,24 +17,6 @@ public class MinAndMax{
         }
         System.out.println(greatest);
     }
-        public static void findMax( Float num1,Float num2,Float num3){
-        Float greatest=num1;
-        if(num2.compareTo(greatest)>0){
-           greatest=num2;
-        }
-                if(num3.compareTo(greatest)>0){
-           greatest=num3;
-        }
-        System.out.println(greatest);
+
+
     }
-                public static void findMax(String name1,String name2,String name3){
-        String greatest=name1;
-        if(name2.compareTo(greatest)>0){
-           greatest=name2;
-        }
-                if(name3.compareTo(greatest)>0){
-           greatest=name3;
-        }
-        System.out.println(greatest);
-    }
-}
