5ad5700036e0936cd14d93212e62824630881bba
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-19 13:36:03 +0530

    feat:created generic class

--- Min&Max/MinAndMax.java
+++ Min&Max/MinAndMax.java
@@ -1,22 +1,38 @@
 public class MinAndMax{
     public static void main(String[] args) {
-       Integer a=20;
-       Integer b=90;
-       Integer c=100; 
-       findMax(a, b, c);
-       findMax(10.0f,11.0f,12.0f);
-       findMax("Leela","Sai","Kumar");
-    }
-    public static <T extends Comparable<T>> void findMax(T num1,T num2,T num3){
-        T greatest=num1;
-        if(num2.compareTo(greatest)>0){
-           greatest=num2;
-        }
-                if(num3.compareTo(greatest)>0){
-           greatest=num3;
-        }
-        System.out.println(greatest);
-    }
+        MaximumTest<Integer> intObj = new MaximumTest<>(10, 20, 30);
 
+        System.out.println("Maximum Integer: " + intObj.testMaximum());
 
+        MaximumTest<Float> floatObj =new MaximumTest<>(11.2f, 54.3f, 8.7f);
+
+        System.out.println("Maximum Float: "  + floatObj.testMaximum());
+
+        MaximumTest<String> stringObj = new MaximumTest<>("Apple", "Peach", "Banana");
+
+        System.out.println("Maximum String: "+ stringObj.testMaximum());
     }
+}
+ class MaximumTest<T extends Comparable<T>>{
+  T x;
+   T y;
+   T z;
+   public MaximumTest(T x, T y, T z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+   }
+   public T testMaximum(){
+    return testMaximum(x,y,z);
+   } 
+   public static <T extends Comparable<T>> T testMaximum(T x, T y, T z){
+        T greatest=x;
+        if(y.compareTo(greatest)>0){
+           greatest=y;
+        }
+        if(z.compareTo(greatest)>0){
+           greatest=z;
+        }
+        return greatest;
+   }
+ }   
