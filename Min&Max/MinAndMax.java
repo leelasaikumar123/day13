70ad3284f2151c9cea2bd2ba7650ad48c5920fb8
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-19 12:39:23 +0530

    feat:greatest number at position 1

--- Min&Max/MinAndMax.java
+++ Min&Max/MinAndMax.java
@@ -1,5 +1,18 @@
 public class MinAndMax{
     public static void main(String[] args) {
-        
+       Integer a=90;
+       Integer b=20;
+       Integer c=80; 
+       findMax(a, b, c);
+    }
+    public static void findMax(Integer num1,Integer num2,Integer num3){
+        Integer greatest=num1;
+        if(num2.compareTo(greatest)>0){
+           greatest=num2;
+        }
+                if(num3.compareTo(greatest)>0){
+           greatest=num3;
+        }
+        System.out.println(greatest);
     }
 }
