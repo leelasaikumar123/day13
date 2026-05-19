2c2bd90392dd7f7b2e7b24b55846035f23fafcbf
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-19 14:02:34 +0530

    feat:printing greatest using a method

--- Min&Max/MinAndMax.java
+++ Min&Max/MinAndMax.java
@@ -1,17 +1,19 @@
 public class MinAndMax{
     public static void main(String[] args) {
-            Integer[] intArr = {10, 20, 30, 40, 50};
+        Integer[] intArr = {10, 20, 30, 40, 50};
         Float[] floatArr = {11.2f, 54.3f, 8.7f, 99.1f};
         String[] stringArr = {"Apple", "Peach", "Banana", "Zoo"};
-        System.out.println("Maximum Integer: " + MaximumTest.testMaximum(intArr));
-        System.out.println("Maximum Float: "  + MaximumTest.testMaximum(floatArr));
-
-        System.out.println("Maximum String: "+ MaximumTest.testMaximum(stringArr));
+        System.out.print("Maximum Integer: ");
+         MaximumTest.testMaximum(intArr);
+        System.out.print("Maximum Float: " );
+        MaximumTest.testMaximum(floatArr);
+        System.out.print("Maximum String: ");
+        MaximumTest.testMaximum(stringArr);
     }
 }
  class MaximumTest<T extends Comparable<T>>{
  
-   public static <T extends Comparable<T>> T testMaximum(T[] values){
+   public static <T extends Comparable<T>> void testMaximum(T[] values){
 T greatest = values[0];
 
   for (T value : values) {
@@ -19,7 +21,9 @@ T greatest = values[0];
                 greatest = value;
             }
         }
-
-        return greatest;
+    printGreatest(greatest);
+    }
+   public static <T> void printGreatest(T greatest){
+    System.out.println(greatest);
    }
  }   
